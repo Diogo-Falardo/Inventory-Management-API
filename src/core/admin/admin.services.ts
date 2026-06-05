86bb35e95766b9e56bee44c8aeb93802cc642290
bloop <diogofalardo04@gmail.com>
2026-06-05 15:27:12 +0100

    updated .gitignore

--- src/core/admin/admin.services.ts
+++
@@ -1,5 +0,0 @@
-import { globalPermissionsService } from "./admin.service";
-
-const use_GlobalPermissionsService = new globalPermissionsService();
-
-export { use_GlobalPermissionsService };
--- src/db/schemas/users/users.types.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { usersSchema } from "./users.schema";
-import { createUser, loginUser, userId } from "./users.dto";
-
-export type type_userSchema = z.infer<typeof usersSchema>;
-export type type_createUser = z.infer<typeof createUser>;
-export type type_loginUser = z.infer<typeof loginUser>;
-export type type_userId = z.infer<typeof userId>;
